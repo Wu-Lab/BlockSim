672db48bc2f263c4967a03f46ccaa342746bb75f
Ling-Yun Wu <wulingyun@gmail.com>
2018-12-05 18:03:19 +0800

    Fix bug in link_block_attack Add block_connectivity function

---
+++ R/analysis.R
@@ -0,0 +1,19 @@
+
+
+#' Calculate block connectivity
+#'
+#'
+#' @export
+block_connectivity <- function(adj_matrix)
+{
+  x <- adj_matrix
+  for (i in 1:ceiling(log2(dim(x)[1])))
+  {
+    x <- x + x %*% x
+    x[x > 0] <- 1
+  }
+  past <- rowSums(x)
+  future <- colSums(x)
+  list(con_mat = x, past = past, future = future)
+}
+
