68bdf5d19afe43bc771546eadfc283001423c6e2
Ling-Yun Wu <wulingyun@gmail.com>
2018-12-19 22:16:00 +0800

    Add maxcord

--- R/analysis.R
+++ R/analysis.R
@@ -1,11 +1,26 @@
+#' Find the faithful order of blocks
+#' 
+#' 
+#' @export
+maxcord <- function(adj_matrix, max_discord = 10)
+{
+  dist_matrix <- shortest_distances(adj_matrix)
+  fb <- find_faithful_blocks(adj_matrix, max_discord, dist_matrix)
+  weight <- numeric(dim(adj_matrix)[1])
+  weight[fb] <- 1
+  past_future <- block_distances(adj_matrix, weight, dist_matrix)
+  order(order(past_future$future, weight, -past_future$past, decreasing = T, na.last = T))
+}
+
+
 #' Find faithful blocks
 #'
 #' @import igraph
 #'
 #' @export
-find_faithful_blocks <- function(adj_matrix, max_discord = 10)
+find_faithful_blocks <- function(adj_matrix, max_discord = 10, dist_matrix = NULL)
 {
-  d <- block_discords(adj_matrix)
+  d <- block_discords(adj_matrix, dist_matrix)
   d[d <= max_discord] <- 0
   d[d > max_discord] <- 1
   largest_ivs(graph_from_adjacency_matrix(d, mode = "undirected"))[[1]]
@@ -16,12 +31,21 @@ find_faithful_blocks <- function(adj_matrix, max_discord = 10)
 #'
 #'
 #' @export
-block_distances <- function(adj_matrix)
+block_distances <- function(adj_matrix, block_weight = NULL, dist_matrix = NULL)
 {
-  x <- shortest_distances(adj_matrix)
-  xx <- x > 0 & x < Inf
-  past <- rowSums(xx)
-  future <- colSums(xx)
+  if (is.null(dist_matrix)) x <- shortest_distances(adj_matrix)
+  else x <- dist_matrix
+  y <- x > 0 & x < Inf
+  if (is.null(block_weight))
+  {
+    past <- rowSums(y)
+    future <- colSums(y)
+  }
+  else
+  {
+    past <- as.vector(y %*% block_weight)
+    future <- as.vector(block_weight %*% y)
+  }
   list(distances = x, past = past, future = future)
 }
 
@@ -30,11 +54,12 @@ block_distances <- function(adj_matrix)
 #'
 #'
 #' @export
-block_discords <- function(adj_matrix)
+block_discords <- function(adj_matrix, dist_matrix = NULL)
 {
+  if (is.null(dist_matrix)) x <- shortest_distances(adj_matrix)
+  else x <- dist_matrix
   n <- dim(adj_matrix)[1]
   discords <- matrix(0, n, n)
-  x <- shortest_distances(adj_matrix)
   d <- apply(x[colSums(adj_matrix) == 0, , drop = F], 2, min)
   past <- lapply(1:n, function(i) x[i, ] < Inf)
   future <- lapply(1:n, function(i) x[, i] < Inf)
